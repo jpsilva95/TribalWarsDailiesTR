ec57189380ec0986c8ac0d4aeebb21995d6cbad5
João Paulo Silva <132902307+jpsilva95@users.noreply.github.com>
2025-11-02 17:07:06 +0000

    Update wrapper.user.js

--- wrapper.user.js
+++ wrapper.user.js
@@ -1,9 +1,10 @@
 // ==UserScript==
-// @name         TeamRocket Dailies
+// @name         Tribal Wars Daily Stats to Discord
 // @namespace    http://tampermonkey.net/
 // @version      1.0
 // @description  Send daily farm and resource stats to Discord webhook
 // @author       TeamRocket
+// @icon         https://i.ibb.co/RptK4TP5/teamrocketlogo.gif
 // @match        https://*.tribalwars.com.pt/game.php*
 // @require      https://raw.githubusercontent.com/jpsilva95/TribalWarsDailiesTR/main/TRDailiesv1.js
 // @updateURL    https://raw.githubusercontent.com/jpsilva95/TribalWarsDailiesTR/main/wrapper.user.js
